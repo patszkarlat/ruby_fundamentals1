6bdea72a0717c9eb832a0b412042fbad84163b2f
patszkarlat <punkgeek@mac.com>
2014-06-03 13:39:38 -0400

    finally committed

---
+++ exercise2.rb
@@ -0,0 +1,10 @@
+# this will print the answer of 55 multiplied by 20.
+puts 55 * 0.20
+# this will 'add' Happy and 5 and insert a space between them.
+puts "Happy" + " " + 5.to_s
+#answer is substituted for the total in the equation
+answer = 45628 * 7839
+puts "The total is #{answer}."
+#(false) OR (false) OR (true); this whole statement is TRUE
+#because OR is only false when all arguments are false.
+(true && false) || (false && true) || !(false && false)
